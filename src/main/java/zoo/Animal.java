8efb650e8a2f0fd342997dc67d22cdbabbfc1a4b
mrsebo24 <sebastian.wiki@wp.pl>
2025-06-08 23:12:03 +0200

    add models

---
+++ src/main/java/zoo/Animal.java
@@ -0,0 +1,34 @@
+package zoo;
+
+abstract class Animal {
+    private String animalName;
+    private Owner owner;
+
+    public Animal(String animalName, Owner owner) {
+        this.animalName = animalName;
+    }
+
+    public String getAnimalName() {
+        return animalName;
+    }
+
+    public void setAnimalName(String animalName) {
+        this.animalName = animalName;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "animalName='" + animalName + '\'' +
+                ", owner=" + owner +
+                '}';
+    }
+}
