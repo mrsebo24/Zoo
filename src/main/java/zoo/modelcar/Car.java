f0dd126750df647de7db64dadaea6de61b9517bf
mrsebo24 <sebastian.wiki@wp.pl>
2025-06-09 11:40:34 +0200

    added constructor to Car

--- src/main/java/zoo/modelcar/Car.java
+++ src/main/java/zoo/modelcar/Car.java
@@ -4,4 +4,8 @@ public class Car {
     private String carMake;
     private String capacityInCm3;
 
+    public Car(String carMake, String capacityInCm3) {
+        this.carMake = carMake;
+        this.capacityInCm3 = capacityInCm3;
+    }
 }
